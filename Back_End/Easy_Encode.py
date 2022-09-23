b4c07eb4e42b42282ab05c5ec27214d8f177ed53
HitKoladiya <koladiyahit45@gmail.com>
2022-09-24 03:46:06 +0530

    encode text in image

--- Back_End/Easy_Encode.py
+++
@@ -1,4 +0,0 @@
-from stegano import lsb
-secret = lsb.hide("./tests/sample-files/Lenna.png", "Hello World")
-secret.save("./Lenna-secret.png")
-clear_message = lsb.reveal("./Lenna-secret.png")
---
+++ Back_End/image_Stegano.py
@@ -0,0 +1,11 @@
+from stegano import lsb
+
+def Encode(src, message):
+    secret=lsb.hide(src,message)
+    secret.save("dest.png")
+
+def Decode(src):
+    print(lsb.reveal('./dest.png'))
+
+Encode("hit.jpg","i am hit koladiya")
+Decode("hit.jpg")
