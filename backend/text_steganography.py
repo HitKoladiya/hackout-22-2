edb16b230047fe00b3f56627d3c60e5c93adda88
hiranj-kotak <95754714+hiranj-kotak@users.noreply.github.com>
2022-09-24 03:51:27 +0530

    intital commit

---
+++ backend/text_steganography.py
@@ -0,0 +1,13 @@
+from stegano import lsb
+
+def encode(img, data):
+    if (len(data) == 0):
+        return ValueError('Data is empty')
+    new_img = lsb.hide(img, data)
+    return  new_img
+
+def decode(img):
+    encrypted_text = lsb.reveal(img)
+    return encrypted_text
+
+
