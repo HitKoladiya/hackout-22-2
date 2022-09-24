3d6bf99c0c56bef68d88e5a8ea344b8cb47ea410
Jay A. Keraliya <jaykeraliya0@gmail.com>
2022-09-24 13:12:04 +0530

    file problem solved

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,54 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 const test = () => {
-    const handleSubmit = (e: any) => {
+
+
+
+    const [file, setFile] = useState();
+
+
+    const handleInput = (e: any) => {
         e.preventDefault();
         const formData = new FormData(e.target);
-
-        const Upload = async () => {
-            await fetch("http://127.0.0.1:5000/image", {
-                method: "POST",
-                body: formData,
-            }).then((resp) => {
-                resp.json().then((data) => {
-                    console.log(data);
-                });
-            });
-        };
         console.log(formData);
 
-        Upload();
-    };
-    return (
-        <form
-            onSubmit={handleSubmit}
-            className="container mt-5 pt-5 pb-5"
-            encType="multipart/form-data"
-        >
-            <div className="form-inline justify-content-center mt-5">
-                <label
-                    htmlFor="image"
-                    className="ml-sm-4 font-weight-bold mr-md-4"
-                >
-                    Image :{" "}
-                </label>
-                <div className="input-group">
-                    <input
-                        type="file"
-                        id="image"
-                        name="file"
-                        accept="image/*"
-                        className="file-custom"
-                    />
-                </div>
-            </div>
+    }
 
-            <div className="input-group justify-content-center mt-4">
-                <button type="submit" className="btn btn-md btn-primary">
-                    Upload,,,
-                </button>
-            </div>
-        </form>
+    const handleFile = async (e: any) => {
+        let file = await e.target.files[0];
+        setFile(file);
+        console.log(file);
+    };
+
+
+    return (
+        <>
+            <form action="">
+                <input type="text" onChange={handleInput} className="" />
+                <input type="file" onChange={handleFile} name="file" id="file" />
+                <input type="submit" value="Upload" />
+            </form>
+        </>
     );
 };
 
