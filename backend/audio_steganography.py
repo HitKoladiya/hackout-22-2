8e1223868f9513c34030e5ed063bcfef967141d5
hiranj-kotak <95754714+hiranj-kotak@users.noreply.github.com>
2022-09-24 14:32:59 +0530

    audio steganography added

---
+++ backend/audio_steganography.py
@@ -0,0 +1,23 @@
+import wave
+
+def encode_audio(string):
+    song = wave.open("base_song.wav" , mode='rb')
+    frame_bytes = bytearray(list(song.readframes(song.getnframes())))
+    string = string + int((len(frame_bytes)-(len(string)*8*8))/8) *'#'
+    bits = list(map(int, ''.join([bin(ord(i)).lstrip('0b').rjust(8,'0') for i in string])))
+    for i, bit in enumerate(bits):
+        frame_bytes[i] = (frame_bytes[i] & 254) | bit
+    frame_modified = bytes(frame_bytes)
+    with wave.open('encoded_song.wav', 'wb') as fd:
+        fd.setparams(song.getparams())
+        fd.writeframes(frame_modified)
+    song.close()
+
+def decode_audio():
+    song = wave.open("encoded_song.wav", mode='rb')
+    frame_bytes = bytearray(list(song.readframes(song.getnframes())))
+    extracted = [frame_bytes[i] & 1 for i in range(len(frame_bytes))]
+    string = "".join(chr(int("".join(map(str, extracted[i:i + 8])), 2)) for i in range(0, len(extracted), 8))
+    decoded = string.split("###")[0]
+    song.close()
+    return decoded
