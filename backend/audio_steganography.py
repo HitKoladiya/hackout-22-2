e7071c03c10277d031624bc8ccb45eda6f76fab1
hiranj-kotak <95754714+hiranj-kotak@users.noreply.github.com>
2022-09-24 22:16:22 +0530

    small updates

--- backend/audio_steganography.py
+++ backend/audio_steganography.py
@@ -1,23 +1,12 @@
 import wave
 def encode_audio(string):
-    # read wave audio file
     song = wave.open("file_example_WAV_1MG.wav", mode='rb')
-    # Read frames and convert to byte array
     frame_bytes = bytearray(list(song.readframes(song.getnframes())))
-
-    # The "secret" text message
-    # Append dummy data to fill out rest of the bytes. Receiver shall detect and remove these characters.
     string = string + int((len(frame_bytes)-(len(string)*8*8))/8) *'#'
-    # Convert text to bit array
     bits = list(map(int, ''.join([bin(ord(i)).lstrip('0b').rjust(8,'0') for i in string])))
-
-    # Replace LSB of each byte of the audio data by one bit from the text bit array
     for i, bit in enumerate(bits):
         frame_bytes[i] = (frame_bytes[i] & 254) | bit
-    # Get the modified bytes
     frame_modified = bytes(frame_bytes)
-
-    # Write bytes to a new wave audio file
     with wave.open('song_embedded.wav', 'wb') as fd:
         fd.setparams(song.getparams())
         fd.writeframes(frame_modified)
@@ -25,14 +14,9 @@ def encode_audio(string):
 
 def decode_audio():
     song = wave.open("song_embedded.wav", mode='rb')
-    # Convert audio to byte array
     frame_bytes = bytearray(list(song.readframes(song.getnframes())))
-
-    # Extract the LSB of each byte
     extracted = [frame_bytes[i] & 1 for i in range(len(frame_bytes))]
-    # Convert byte array back to string
     string = "".join(chr(int("".join(map(str, extracted[i:i + 8])), 2)) for i in range(0, len(extracted), 8))
-    # Cut off at the filler characters
     decoded = string.split("###")[0]
     song.close()
     return decoded
