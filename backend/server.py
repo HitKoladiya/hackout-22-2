66b0d61fbf7d82e64a94fa9dc405aea6a676191a
hiranj-kotak <95754714+hiranj-kotak@users.noreply.github.com>
2022-09-24 22:50:12 +0530

    fix in audio files

--- backend/server.py
+++ backend/server.py
@@ -2,7 +2,7 @@ from flask import  Flask,request,send_file
 from flask_cors import CORS
 from ipfs import recieve_data_encode,recieve_data_decode
 from audio_ipfs import  recieve_audio_data_encode,recieve_audio_data_decode
-from text_to_img_ipfs import recieve_text_data_encode,recieve_text_data_decode
+from text_to_img_ipfs import recieve_data_decode_txt,recieve_data_encode_txt
 
 app = Flask(__name__)
 CORS(app)
@@ -53,7 +53,7 @@ def process_audio_decode():
 def process_txt_encode():
     if request.method == 'POST':
         data = request.get_json()
-        id = recieve_text_data_encode(data)
+        id = recieve_data_encode_txt(data)
         return {
             "status": "200",
             "IpfsHash" : id
@@ -63,7 +63,7 @@ def process_txt_encode():
 def process_txt_decode():
     if request.method == 'POST':
         data = request.get_json()
-        id = recieve_text_data_decode(data)
+        id = recieve_data_decode_txt(data)
         return {
             "status": "200",
             "decoded_text": id
