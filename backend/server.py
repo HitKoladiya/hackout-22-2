8464ce018aae35a988c85ff16d93893727a4db85
hiranj-kotak <95754714+hiranj-kotak@users.noreply.github.com>
2022-09-24 10:48:02 +0530

    error solved

---
+++ backend/server.py
@@ -0,0 +1,28 @@
+from flask import  Flask,request,send_file
+from PIL import Image
+from flask_cors import CORS
+from text_steganography import  encode,decode
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def hello_world():
+    return "hello world"
+
+@app.route("/image",methods = ["post","get"])
+def process():
+    if request.method == 'POST':
+        # check if the post
+        # request has the file part
+        print(request)
+        f = request.files.get('file')
+        img = Image.open(f)
+        print(img)
+        img.save("hello.png")
+        encode(img,"fuck off")
+        return send_file("./hello.png")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
